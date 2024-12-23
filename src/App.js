19c851d6c8d58037bdf3d2ab6f270b95121b6368
Paddy Ryan <paddy@Paddys-MacBook-Pro.local>
2024-12-23 13:11:54 +0000

    initial commit v2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          WHAT IS HAPPENING HERE
         </a>
       </header>
     </div>
